ad52ebe718fd07f0fa4cea942b1543b51187caa6
nikit696 <vqwerty2099@gmail.com>
2022-01-08 19:13:13 +0300

    add new tasks

---
+++ 54.js
@@ -0,0 +1,10 @@
+function myLocalScope() {
+    // Only change code below this line
+    const myVar = "good"
+    console.log('inside myLocalScope', myVar);
+  }
+  myLocalScope();
+  
+  // Run and check the console
+  // myVar is not defined outside of myLocalScope
+  
