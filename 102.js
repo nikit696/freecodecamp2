1c395dfa328d5fefd72e68b020ecca13443ca828
nikit696 <vqwerty2099@gmail.com>
2022-01-10 18:11:08 +0300

    first part

---
+++ 102.js
@@ -0,0 +1,13 @@
+function multiplyAll(arr) {
+    let product = 1;
+    // Only change code below this line
+    for (let i = 0; i < arr.length; i++){
+      for(let k = 0; k < arr[i].length; k++){
+        product *= arr[i][k];
+      }
+    }
+    // Only change code above this line
+    return product;
+  }
+  
+  multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
